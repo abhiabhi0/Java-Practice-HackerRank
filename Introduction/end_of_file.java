db74f8f2fd220a835f1d6ba0240755b60ff267c9
Abhishek Singh Thakur <mailabhi593@gmail.com>
2022-08-20 17:01:57 +0530

    Create end_of_file.java

---
+++ Introduction/end_of_file.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        String s = "";
+        
+        int count = 1;
+        
+        while (scan.hasNext())
+        {
+            s = scan.nextLine();
+            System.out.print(count);
+            System.out.print(" ");
+            System.out.println(s);
+            count++;
+        }
+    }
+}
